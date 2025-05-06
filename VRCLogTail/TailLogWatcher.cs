529267523432bbad22d74adab295e4f6aadfdc5b
koturn <jeak.koutan.apple@gmail.com>
2025-05-07 02:31:34 +0900

    feat: Update library: Koturn.VRChat.Log: v1.3.0 -> v1.5.0

--- VRCLogTail/TailLogWatcher.cs
+++ VRCLogTail/TailLogWatcher.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 #if NET5_0_OR_GREATER
 using System.Runtime.InteropServices;
 #endif  // NET5_0_OR_GREATER
+#if NET9_0_OR_GREATER
+using System.Threading;
+#endif  // NET9_0_OR_GREATER
 using Koturn.VRChat.Log;
 using Koturn.VRChat.Log.Enums;
 #if !NET5_0_OR_GREATER
 using VRCLogTail.Internals;
 #endif  // !NET5_0_OR_GREATER
+
+#if !NET9_0_OR_GREATER
+using Lock = object;
+#endif  // !NET9_0_OR_GREATER
 
 
 namespace VRCLogTail
@@ -37,6 +44,10 @@ namespace VRCLogTail
         private sealed class TailLogParser(string filePath) : VRCBaseLogParser(filePath)
         {
             /// <summary>
+            /// Lock object for <see cref="Console.Out"/>.
+            /// </summary>
+            private static readonly Lock _lock = new();
+            /// <summary>
             /// Console colors.
             /// </summary>
             private static readonly ConsoleColor[] _consoleColor = [
@@ -50,14 +61,11 @@ namespace VRCLogTail
             /// <summary>
             /// Load one log item and output to stdout.
             /// </summary>
-            /// <param name="logAt">Log timestamp.</param>
             /// <param name="level">Log level.</param>
             /// <param name="logLines">Log lines.</param>
             /// <returns>True if any of the log parsing defined in this class succeeds, otherwise false.</returns>
-            protected override bool OnLogDetected(DateTime logAt, VRCLogLevel level, List<string> logLines)
+            protected override bool OnLogDetected(VRCLogLevel level, List<string> logLines)
             {
-                Console.ForegroundColor = _consoleColor[(int)level];
-
                 var writer = Console.Out;
 #if NETCOREAPP2_1_OR_GREATER
 #if NET5_0_OR_GREATER
@@ -65,23 +73,32 @@ namespace VRCLogTail
 #else
                 var logLineSpan = ListHelper.AsSpan(logLines);
 #endif  // NET5_0_OR_GREATER
-                writer.WriteLine($@"[{DateTimeUtil.FormatDateTime(logAt)}][{level.GetName()}] {logLineSpan[0]}");
-                foreach (var line in logLineSpan.Slice(1))
+                lock (_lock)
                 {
-                    writer.WriteLine(line);
+                    Console.ForegroundColor = _consoleColor[(int)level];
+                    writer.WriteLine($@"[{DateTimeUtil.FormatDateTime(LogAt)}][{level.GetName()}] {logLineSpan[0]}");
+                    foreach (var line in logLineSpan.Slice(1))
+                    {
+                        writer.WriteLine(line);
+                    }
+                    writer.Flush();
+                    Console.ForegroundColor = ConsoleColor.Gray;
                 }
 #else
                 var logLineArray = ListHelper.GetArray(logLines);
-                writer.WriteLine($"[{DateTimeUtil.FormatDateTime(logAt)}][{level.GetName()}] {logLineArray[0]}");
                 var count = logLines.Count;
-                for (int i = 1; i < count; i++)
+                lock (_lock)
                 {
-                    writer.WriteLine(logLineArray[i]);
+                    Console.ForegroundColor = _consoleColor[(int)level];
+                    writer.WriteLine($"[{DateTimeUtil.FormatDateTime(LogAt)}][{level.GetName()}] {logLineArray[0]}");
+                    for (int i = 1; i < count; i++)
+                    {
+                        writer.WriteLine(logLineArray[i]);
+                    }
+                    writer.Flush();
+                    Console.ForegroundColor = ConsoleColor.Gray;
                 }
 #endif  // NETCOREAPP2_1_OR_GREATER
-                writer.Flush();
-
-                Console.ForegroundColor = ConsoleColor.Gray;
 
                 return true;
             }
